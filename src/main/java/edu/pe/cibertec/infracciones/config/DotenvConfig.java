97642ec504d6225e3247feeaef71056220335b5e
Ganpiero <ganpieromaiketreyeszavaleta@gmail.com>
2026-03-31 00:31:06 -0500

    establecido para testing

--- src/main/java/edu/pe/cibertec/infracciones/config/DotenvConfig.java
+++ src/main/java/edu/pe/cibertec/infracciones/config/DotenvConfig.java
@@ -1,10 +1,16 @@
 package edu.pe.cibertec.infracciones.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Configuration
+@Profile("!test")
 public class DotenvConfig {
 
-    static {
+    @PostConstruct
+    public void loadEnv() {
         Dotenv dotenv = Dotenv.configure()
                 .ignoreIfMissing()
                 .load();
